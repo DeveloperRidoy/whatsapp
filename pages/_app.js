1821b3fef9e6a6982fd9e0f02b03d8419d0b57c6
DeveloperRidoy <freelancerridoy247@gmail.com>
2021-05-27 19:14:50 +0600

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="max-w-[1500px] mx-auto">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp
