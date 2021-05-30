642b34be0a37661272a2ad3473e921d5dc7e487f
DeveloperRidoy <freelancerridoy247@gmail.com>
2021-05-30 21:20:42 +0600

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 
 module.exports = {
   mode: 'jit',
@@ -5,6 +7,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        emerald: colors.emerald
+      }
     },
   },
   variants: {
