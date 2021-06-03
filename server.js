9a3c3993bdc275c8e0b7f8506eb47d926984b139
DeveloperRidoy <freelancerridoy247@gmail.com>
2021-06-03 20:14:08 +0600

    initial commit

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app)
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
 const next = require('next');
 const dotenv = require('dotenv');
 const cookieParser = require("cookie-parser");
 const compression = require("compression");
 const morgan = require("morgan");
 const socket = require('./socket/socket');
-
+const cors = require('cors');
+const { instrument } = require('@socket.io/admin-ui');
+const bcrypt = require('bcrypt');
 
 // environmental variables
 dotenv.config({ path: `${__dirname}/.env.local` });
@@ -24,8 +26,29 @@ nextApp
     // trust proxy
     app.enable("trust proxy");
 
+    // cosr 
+    app.use(cors({ origin: ["https://admin.socket.io"], optionsSuccessStatus: 200 }));
+
     // socket.io connection
     socket(io);
+
+    // socket.io admin ui 
+    bcrypt.hash(process.env.SOCKET_ADMIN_PASSWORD, 12, (err, hash) => {
+      if (err) {
+        console.log("shutting down server on error");
+        console.log(err);
+        process.exit(1);
+      }
+      const username = process.env.SOCKET_ADMIN_USERNAME;
+      const password = hash;
+      instrument(io, {
+        auth: {
+          type: "basic",
+          username,
+          password,
+        },
+      });
+    });
 
     // show api requests info in development mode
     if (process.env.NODE_ENV !== "production") {
